75c4df3b9f024abbd41499dded3b855e57ca8813
kimdaewonn <eodnjs9605@gmail.com>
2023-09-03 23:54:29 +0900

    vuestagram

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <Post :instadata="instadata"/>
+    <Post :instadata="instadata"/>
+    <Post :instadata="instadata"/>
+  </div>
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+props: {
+  instadata: Array,
+},
+
+components: {
+  Post,
+}
+
+}
+</script>
+
+<style>
+</style>
